0be7e5e3947370528132f300ac6cdc8b69b4c75f
Yaminbens <yaminbs@gmail.com>
2019-03-16 18:59:32 +0200

    updated for full coverage

--- selectivnet_utils.py
+++ selectivnet_utils.py
@@ -37,7 +37,28 @@ def calc_selective_risk(model, regression, calibrated_coverage=None):
         loss = np.sum(y_hat[covered_idx] != np.argmax(model.y_test[covered_idx, :], 1)) / np.sum(covered_idx)
     return loss, coverage
 
+def train_full_coverage(model_name, model_cls, coverages, model_baseline=None, regression=False, alpha=0.5):
+    results = {}
+    print("training model with full coverage")
+    coverage_rate = 1.
+    model = model_cls(train=True,
+                      filename="{}_{}.h5".format(model_name, coverage_rate),
+                      coverage=coverage_rate,
+                      alpha=alpha)
 
+    loss, coverage = calc_selective_risk(model, regression)
+    results[coverage] = {"lambda": coverage_rate, "selective_risk": loss}
+
+    if model_baseline is not None:
+        if regression:
+            results[coverage]["baseline_risk"] = (model_baseline.selective_risk_at_coverage(coverage))
+
+        else:
+
+            results[coverage]["baseline_risk"] = (1 - model_baseline.selective_risk_at_coverage(coverage))
+        results[coverage]["percentage"] = 1 - results[coverage]["selective_risk"] / results[coverage]["baseline_risk"]
+
+    save_dict("results/{}.json".format(model_name), results)
 def train_profile(model_name, model_cls, coverages, model_baseline=None, regression=False, alpha=0.5):
     results = {}
     for coverage_rate in coverages:
@@ -90,32 +111,3 @@ def my_generator(func, x_train, y_train, batch_size, k=10):
         yield [res[0], [res[1], res[1][:, :-1]]]
 
 
-def create_cats_vs_dogs_npz(cats_vs_dogs_path='datasets'):
-    labels = ['cat', 'dog']
-    label_to_y_dict = {l: i for i, l in enumerate(labels)}
-
-    def _load_from_dir(dir_name):
-        glob_path = os.path.join(cats_vs_dogs_path, dir_name, '*.*.jpg')
-        imgs_paths = glob(glob_path)
-        images = [resize_and_crop_image(p, 64) for p in imgs_paths]
-        x = np.stack(images)
-        y = [label_to_y_dict[os.path.split(p)[-1][:3]] for p in imgs_paths]
-        y = np.array(y)
-        return x, y
-
-    x_train, y_train = _load_from_dir('train')
-    x_test, y_test = _load_from_dir('test')
-
-    np.savez_compressed(os.path.join(cats_vs_dogs_path, 'cats_vs_dogs.npz'),
-                        x_train=x_train, y_train=y_train,
-                        x_test=x_test, y_test=y_test)
-
-
-def load_cats_vs_dogs(cats_vs_dogs_path='datasets/'):
-    npz_file = np.load(os.path.join(cats_vs_dogs_path, 'cats_vs_dogs.npz'))
-    x_train = npz_file['x_train']
-    y_train = npz_file['y_train']
-    x_test = npz_file['x_test']
-    y_test = npz_file['y_test']
-
-    return (x_train, y_train), (x_test, y_test)
