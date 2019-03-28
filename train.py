fa9483c0c8f451b081cbe023bbaa0c7de65f4f5d
Yaminbs <yaminbs@gmail.com>
2019-03-28 19:23:12 +0200

    changing risk with l=0.5

--- train.py
+++ train.py
@@ -13,8 +13,8 @@ parser.add_argument('--dataset', type=str, default='cifar_10')
 parser.add_argument('--model_name', type=str, default='coverage_loss')
 parser.add_argument('--baseline', type=str, default='none')
 parser.add_argument('--alpha', type=float, default=0.5)
-parser.add_argument('--llambda', type=float, default=1/32)
-parser.add_argument('--risk', type=float, default=0.03)
+parser.add_argument('--llambda', type=float, default=1/2)
+parser.add_argument('--risk', type=float, default=0.048)
 
 args = parser.parse_args()
 
